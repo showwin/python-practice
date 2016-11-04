47a3fd6d72ce705e64cce9d39187da92c05db180
showwin <showwin_kmc@yahoo.co.jp>
2016-11-04 20:22:28 +0900

    add clustering

--- collective_intelligence/clusters.py
+++ collective_intelligence/clusters.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from math import sqrt
+from PIL import Image, ImageDraw
+import time
+import sys
+import random
 
 
 def readfile(filename):
@@ -89,8 +93,162 @@ def hcluster(rows, distance=pearson):
     return clust[0]
 
 
-# データ読み込み
-# blognames, words, data = clusters.readfile('./data/cluster/blogdata.txt')
+def printclust(clust, labels=None, n=0):
+    # 階層型のレイアウトにするためにインデントする
+    for i in range(n):
+        sys.stdout.write('  ')
+    if clust.id < 0:
+        # 枝の情報
+        print('-')
+    else:
+        # 葉 (終端)
+        if labels is None:
+            print(clust.id)
+        else:
+            print(labels[clust.id])
 
-# クラスタ作成
-# clust = clusters.hcluster(data)
+    if clust.left is not None:
+        printclust(clust.left, labels, n=n + 1)
+    if clust.right is not None:
+        printclust(clust.right, labels, n=n + 1)
+
+
+def getHeight(clust):
+    if clust.left is None and clust.right is None:
+        return 1
+
+    return getHeight(clust.left) + getHeight(clust.right)
+
+
+def getDepth(clust):
+    # 終端への距離は0.0
+    if clust.left is None and clust.right is None:
+        return 0
+
+    # 枝の距離は2つの方向の大きい方にそれ自身の距離を足したもの
+    return max(getDepth(clust.left), getDepth(clust.right)) + clust.distance
+
+
+def drawdendrogram(clust, labels, jpeg='clusters.jpg'):
+    # 高さと幅
+    h = getHeight(clust) * 20
+    w = 1200
+    depth = getDepth(clust)
+
+    # 幅は固定されているため、適宜縮尺する
+    scaling = float(w - 150)/depth
+
+    # 白を背景とする新しい画像を作る
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.line((0, h/2, 10, h/2), fill=(255, 0, 0))
+
+    # 最初のノードを書く
+    drawnode(draw, clust, 10, (h/2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+
+
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = getHeight(clust.left) * 20
+        h2 = getHeight(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+        # 直線の長さ
+        ll = clust.distance*scaling
+
+        # クラスタから子への垂直な直線
+        draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill=(255, 0, 0))
+
+        # 左側のアイテムへの水平な直線
+        draw.line((x, top + h1 / 2, x + ll, top + h1 / 2), fill=(255, 0, 0))
+
+        # 右側のアイテムへの水平な直線
+        draw.line((x, bottom - h2 / 2, x + ll, bottom - h2 / 2),
+                  fill=(255, 0, 0))
+
+        # 左右のノードたちを描く関数を呼び出す
+        drawnode(draw, clust.left, x + ll, top + h1 / 2, scaling, labels)
+        drawnode(draw, clust.right, x + ll, bottom - h2 / 2, scaling, labels)
+
+    else:
+        # 終点であればアイテムのラベルを描く
+        draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
+
+def rotatematrix(data):
+    newdata = []
+    for i in range(len(data[0])):
+        newrow = [data[j][i] for j in range(len(data))]
+        newdata.append(newrow)
+    return newdata
+
+
+def kcluster(rows, distance=pearson, k=4):
+    # それぞれのポイントの最小値と最大値を決める
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+
+    # 重心をランダムにk個配置する
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches = None
+
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches = [[] for i in range(k)]
+
+        # それぞれの行に対して、もっとも近い重心を探し出す
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row):
+                    bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches == lastmatches:
+            break
+        lastmatches = bestmatches
+
+        # 重心をそのメンバーの平均に移動する
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+
+    return bestmatches
+
+# データ読み込み
+print('Loading Data...')
+loadstart = time.time()
+blognames, words, data = readfile('./data/cluster/blogdata.txt')
+print(str(time.time() - loadstart) + ' sec')
+
+# ブログ記事のクラスタ作成
+print('construct blog clustering...')
+clustering_start = time.time()
+clust = hcluster(data)
+print(str(time.time() - clustering_start) + ' sec')
+
+# printclust(clust, labels=blognames)
+drawdendrogram(clust, blognames, jpeg='blogclust.jpg')
+
+# 単語のクラスタ生成
+'''
+print('construct word clustering...')
+clustering_start = time.time()
+rdata = rotatematrix(data)
+wordclust = hcluster(rdata)
+print(str(time.time() - clustering_start) + ' sec')
+drawdendrogram(wordclust, words, jpeg='wordclust.jpg')
+'''
+
+kclust = kcluster(data, k=10)
