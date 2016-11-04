7245d86aaba9a324bb8b8cf130c64de42fe9d902
showwin <showwin_kmc@yahoo.co.jp>
2016-11-04 12:33:01 +0900

    add cluster

---
+++ collective_intelligence/clusters.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from math import sqrt
+
+
+def readfile(filename):
+    lines = [line for line in file(filename)]
+
+    # 最初の行はタイ凸
+    colnames = lines[0].strip().split('\t')[1:]
+    rownames = []
+    data = []
+    for line in lines[1:]:
+        p = line.strip().split('\t')
+        # それぞれの行の最初の列は行の名前
+        rownames.append(p[0])
+        # 残りの部分がその行のデータ
+        data.append([float(x) for x in p[1:]])
+    return rownames, colnames, data
+
+
+# 1 - pearson にすることで、関連が強いほど0に近い値になる
+def pearson(v1, v2):
+    sum1 = sum(v1)
+    sum2 = sum(v2)
+
+    sum1Sq = sum([pow(v, 2) for v in v1])
+    sum2Sq = sum([pow(v, 2) for v in v2])
+
+    pSum = sum([v1[i] * v2[i] for i in range(len(v1))])
+
+    num = pSum - (sum1*sum2/len(v1))
+    den = sqrt((sum1Sq - pow(sum1, 2)/len(v1)) *
+               (sum2Sq - pow(sum2, 2)/len(v1)))
+    if den == 0:
+        return 0
+
+    return 1.0 - num/den
+
+
+class bicluster:
+    def __init__(self, vec, left=None, right=None, distance=0.0, id=None):
+        self.left = left
+        self.right = right
+        self.vec = vec
+        self.id = id
+        self.distance = distance
+
+
+def hcluster(rows, distance=pearson):
+    distances = {}
+    currentclustid = -1
+
+    # クラスタは最初は行たち
+    clust = [bicluster(rows[i], id=i) for i in range(len(rows))]
+
+    while len(clust) > 1:
+        lowestpair = (0, 1)
+        closest = distance(clust[0].vec, clust[1].vec)
+
+        # 全ての組をループして、最も距離の近い組を探す
+        for i in range(len(clust)):
+            for j in range(i+1, len(clust)):
+                if (clust[i].id, clust[j].id) not in distances:
+                    distances[(clust[i].id, clust[j].id)] = \
+                        distance(clust[i].vec, clust[j].vec)
+
+                d = distances[(clust[i].id, clust[j].id)]
+
+                if d < closest:
+                    closest = d
+                    lowestpair = (i, j)
+
+        # 2つのクラスタの平均を計算する
+        mergevec = [
+            (clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i]) / 2.0
+            for i in range(len(clust[0].vec))
+        ]
+
+        # 新たなクラスタを作る
+        newcluster = bicluster(mergevec, left=clust[lowestpair[0]],
+                               right=clust[lowestpair[1]], distance=closest,
+                               id=currentclustid)
+
+        currentclustid -= 1
+        del clust[lowestpair[0]]
+        del clust[lowestpair[1] - 1]
+        clust.append(newcluster)
+
+    return clust[0]
+
+
+# データ読み込み
+# blognames, words, data = clusters.readfile('./data/cluster/blogdata.txt')
+
+# クラスタ作成
+# clust = clusters.hcluster(data)
